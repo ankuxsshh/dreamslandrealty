0a2b697e909b9f395023192056a4dcec2e74c40c
ANKUSH SUNIL <137211060+ankuxsshh@users.noreply.github.com>
2025-03-04 09:28:51 +0530

    Update settings.py

--- dreamland_realty/settings.py
+++ dreamland_realty/settings.py
@@ -78,15 +78,10 @@ WSGI_APPLICATION = "dreamland_realty.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dreamslandrealty',       # Your database name
-        'USER': 'postgres',          # Your PostgreSQL username
-        'PASSWORD': '5229',  # Your PostgreSQL password
-        'HOST': 'localhost',         # Database host, usually localhost
-        'PORT': '5432',              # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
